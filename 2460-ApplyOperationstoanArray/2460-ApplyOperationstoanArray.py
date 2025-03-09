c372dd0bafc075e1ef5463f338aae0f88e0773aa
Shruti Joshi <joshishruti009@gmail.com>
2025-03-09 12:49:31 -0700

    File updated at 3/9/2025, 12:49:30 PM

--- 2460-ApplyOperationstoanArray/2460-ApplyOperationstoanArray.py
+++ 2460-ApplyOperationstoanArray/2460-ApplyOperationstoanArray.py
@@ -10,10 +10,8 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[k] = nums[i]
+                if i != k:
+                    nums[i] = 0
                 k += 1
-
-        for i in range(k,len(nums)):
-            nums[k] = 0
-            k += 1
 
         return nums   
