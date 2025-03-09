8f7c6886b750130b6df15fd63b6dfe4e4f0844b7
Shruti Joshi <joshishruti009@gmail.com>
2025-03-09 13:46:05 -0700

    File updated at 3/9/2025, 1:46:04 PM

--- 657-RobotReturntoOrigin/657-RobotReturntoOrigin.py
+++ 657-RobotReturntoOrigin/657-RobotReturntoOrigin.py
@@ -1,10 +1,6 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        directions = {'R':(0,1),'L':(0,-1),'U':(1,0),'D':(-1,0)}
-        x = y = 0 
-        for i in range(len(moves)):
-            p1, p2 = directions[moves[i]]
-            x = x + p1
-            y = y + p2
+        horizontal = moves.count("U") == moves.count("D") 
+        vertical = moves.count("L") == moves.count("R") 
+        return(horizontal and vertical)
         
-        return x == 0 and y == 0
