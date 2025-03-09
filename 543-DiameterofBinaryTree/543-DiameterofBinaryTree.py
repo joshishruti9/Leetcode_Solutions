3608cb62317e82110302a7baaac8a713bf450e40
Shruti Joshi <joshishruti009@gmail.com>
2025-03-09 12:09:42 -0700

    Create file

---
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def traverse(self,node):
+        if node.left is None and node.right is None:
+            return 1
+        
+        l_len = r_len = 0
+
+        if node.left:
+            l_len = self.traverse(node.left)
+        if node.right:
+            r_len = self.traverse(node.right)
+        
+        self.max = max(self.max, l_len + r_len)
+
+        return max(l_len,r_len) + 1
+
+
+
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        self.max = 0
+        max_len = self.traverse(root)
+
+        return self.max
+        
