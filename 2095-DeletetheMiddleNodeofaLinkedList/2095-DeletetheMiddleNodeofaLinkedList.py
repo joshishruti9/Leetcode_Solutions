6000a262c054168549259865b70cd3e8bf4a5fc8
Shruti Joshi <joshishruti009@gmail.com>
2025-03-30 17:38:44 -0700

    File updated at 3/30/2025, 5:38:43 PM

--- 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.py
+++ 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.py
@@ -1,32 +1,27 @@
-# Last updated: 3/30/2025, 5:31:07 PM
+# Last updated: 3/30/2025, 5:38:43 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
-    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
-            return None
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        
+        n = 0
+        arr = []
 
-        prev_odd_head = ListNode()
-        prev_even_head = ListNode()
-        curr_even = prev_even_head
-        curr_odd = prev_odd_head
+        while head:
+            n += 1
+            arr.append(head.val)
+            head = head.next
+        
+        max_sum = 0
+        p1 = 0
+        p2 = n-1
 
-        node = head
-        count  = 0
-
-        while node:
-            if (count % 2) == 0:
-                curr_odd.next = node
-                curr_odd = curr_odd.next
-            else:
-                curr_even.next = node
-                curr_even = curr_even.next
-            count += 1
-            node = node.next
-        curr_even.next = None
-        curr_odd.next = prev_even_head.next
-
-        return prev_odd_head.next
+        while p1 < p2:
+            max_sum = max(max_sum, (arr[p1] + arr[p2]))
+            p1 += 1
+            p2 -= 1
+        
+        return max_sum
