cd8261e1dca37d7a0243bf55d6395ebbf8f741e7
Shruti Joshi <joshishruti009@gmail.com>
2025-03-30 17:31:08 -0700

    File updated at 3/30/2025, 5:31:07 PM

--- 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.py
+++ 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.py
@@ -1,30 +1,32 @@
-# Last updated: 3/30/2025, 5:07:38 PM
+# Last updated: 3/30/2025, 5:31:07 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
-    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head.next:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
             return None
-            
-        n = 0
-        node = head
-        while node:
-            n += 1
-            node = node.next
-            
-        mid = n // 2
+
+        prev_odd_head = ListNode()
+        prev_even_head = ListNode()
+        curr_even = prev_even_head
+        curr_odd = prev_odd_head
 
         node = head
-        count = 1
+        count  = 0
+
         while node:
-            if count == mid:
-                node.next = node.next.next
-                break
+            if (count % 2) == 0:
+                curr_odd.next = node
+                curr_odd = curr_odd.next
             else:
-                node = node.next
+                curr_even.next = node
+                curr_even = curr_even.next
             count += 1
+            node = node.next
+        curr_even.next = None
+        curr_odd.next = prev_even_head.next
 
-        return head
+        return prev_odd_head.next
