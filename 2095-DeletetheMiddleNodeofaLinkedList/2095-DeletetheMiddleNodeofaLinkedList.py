d78581eeb64dfdb90dde4250d1d84431bd18fd54
Shruti Joshi <joshishruti009@gmail.com>
2025-03-30 17:53:45 -0700

    File updated at 3/30/2025, 5:53:44 PM

--- 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.py
+++ 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.py
@@ -1,27 +1,25 @@
-# Last updated: 3/30/2025, 5:38:43 PM
+# Last updated: 3/30/2025, 5:53:44 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
-    def pairSum(self, head: Optional[ListNode]) -> int:
-        
-        n = 0
-        arr = []
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return None
 
-        while head:
-            n += 1
-            arr.append(head.val)
-            head = head.next
-        
-        max_sum = 0
-        p1 = 0
-        p2 = n-1
+        prev_node = head
+        node = prev_node.next
 
-        while p1 < p2:
-            max_sum = max(max_sum, (arr[p1] + arr[p2]))
-            p1 += 1
-            p2 -= 1
+        while node:
+            temp = node.next
+            node.next = prev_node
+            prev_node = node
+            node = temp
+
+        head.next = None
+        return prev_node
         
-        return max_sum
+        
+        
