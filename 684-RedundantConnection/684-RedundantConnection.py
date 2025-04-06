ec99c16b20be83c0dd8a6599363b455629ab21a9
Shruti Joshi <joshishruti009@gmail.com>
2025-04-06 14:38:51 -0700

    Create file

---
+++ 684-RedundantConnection/684-RedundantConnection.py
@@ -0,0 +1,38 @@
+# Last updated: 4/6/2025, 2:38:50 PM
+class Solution:
+
+    def find(self, x, root):
+        if root[x] == x:
+            return x
+        return self.find(root[x], root)
+
+    def union(self, x, y, rank, root):
+
+        rx = self.find(x, root)
+        ry = self.find(y, root)
+
+        if rx == ry:
+            return False
+        
+        if rank[rx] > rank[ry]:
+            root[ry] = rx
+        elif rank[rx] < rank[ry]:
+            root[rx] = ry
+        else:
+            rank[rx] += 1
+            root[ry] = rx
+
+        return True
+
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
+        n = len(edges)
+        root = [i for i in range(n+1)]
+        rank = [1 for i in range(n+1)]
+        output = []
+
+        for x,y in edges:
+            if not self.union(x, y, rank, root):
+                output = [x,y]
+
+        return output
