cc2e5e9f56733fc0d282c7d87c8aecb55edc73cf
Shruti Joshi <joshishruti009@gmail.com>
2025-04-08 14:10:27 -0700

    File updated at 4/8/2025, 2:10:28 PM

--- 77-Combinations/77-Combinations.py
+++ 77-Combinations/77-Combinations.py
@@ -1,21 +1,20 @@
-# Last updated: 4/8/2025, 1:54:30 PM
+# Last updated: 4/8/2025, 2:10:28 PM
 class Solution:
     def traverse(self, n, k, curr_num, curr_output):
         if len(curr_output) == k:
-            self.output.append(curr_output[:])
+            self.output.append(curr_output)
             return
 
         for i in range(curr_num, n+1):
-            curr_output.append(i)
-            self.traverse(n, k, i+1, curr_output)
-            curr_output.pop()
+            new_output = curr_output[:]
+            new_output.append(i)
+            self.traverse(n, k, i+1, new_output)
+            
         return
    
-
     def combine(self, n: int, k: int) -> List[List[int]]:
         self.output = []
 
         self.traverse(n, k, 1, []) 
 
         return self.output
-        
