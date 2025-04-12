465b75741f6919f041abed630cb8829430378d47
Shruti Joshi <joshishruti009@gmail.com>
2025-04-12 15:34:51 -0700

    File updated at 4/12/2025, 3:34:51 PM

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.py
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.py
@@ -1,28 +1,25 @@
-# Last updated: 4/12/2025, 3:30:01 PM
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
-class RecentCounter:
+# Last updated: 4/12/2025, 3:34:51 PM
+from collections import deque
 
-    def __init__(this):
-        this.requests = []
-        this.counter = None
+class RecentCounter:
+    
+    def __init__(self):
+        self.queue = deque()
+
+    def ping(self, t: int) -> int:
+        count = 0
+        self.queue.append(t)
+        range_start = t - 3000
+        range_end = t
+
+        while self.queue and range_start > self.queue[0]:
+            self.queue.popleft()
+        
+        return len(self.queue)
+        
         
 
-    def ping(this, t: int) -> int:
-        this.requests.append(t)
-        if(this.counter == None):
-            this.counter = t
-        else:
-            this.counter = this.requests[-1] - this.requests[0]
 
-
-        while(this.counter > 3000 and len(this.requests) > 1):
-            # print(this.counter)
-            f = this.requests.pop(0)
-            if(len(this.requests) >= 2):
-                this.counter = this.requests[-1] - this.requests[0]
-            else:
-                this.counter = this.requests[0]
-
-        return len(this.requests)
-
-   
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
