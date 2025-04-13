90de58be6606c8e9e94ca17f51bea8b03fbac4ec
Shruti Joshi <joshishruti009@gmail.com>
2025-04-13 15:39:54 -0700

    File updated at 4/13/2025, 3:39:54 PM

--- 404-SumofLeftLeaves/404-SumofLeftLeaves.py
+++ 404-SumofLeftLeaves/404-SumofLeftLeaves.py
@@ -1,4 +1,4 @@
-# Last updated: 4/13/2025, 3:37:59 PM
+# Last updated: 4/13/2025, 3:39:54 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,22 +6,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def traverse(self, node, flag):
+    def traverse(self, node, flag, total):
         
         if node.left is None and node.right is None and flag is True:
-            self.total += node.val
-            return
+            total += node.val
+            return total
+
+        ltotal = rtotal = 0
 
         if node.left:
-            self.traverse(node.left,True)
+            ltotal = self.traverse(node.left,True, total)
         if node.right:
-            self.traverse(node.right,False)
+            rtotal = self.traverse(node.right,False, total)
+
+        return ltotal + rtotal
 
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
 
-        self.total = 0
-        self.traverse(root, False)
+        total = 0
+        total = self.traverse(root, False, total)
 
-        return self.total
+        return total
