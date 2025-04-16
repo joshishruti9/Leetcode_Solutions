bf976514691405f3bcd39edbbd1c17a848111837
Shruti Joshi <joshishruti009@gmail.com>
2025-04-16 13:56:12 -0700

    File updated at 4/16/2025, 1:56:12 PM

--- 338-CountingBits/338-CountingBits.py
+++ 338-CountingBits/338-CountingBits.py
@@ -1,17 +1,25 @@
-# Last updated: 4/16/2025, 1:45:11 PM
+# Last updated: 4/16/2025, 1:56:12 PM
 class Solution:
-    def count_one(self, n, count):
-        if n == 0:
-            return count
-        if n % 2 == 1:
-            count += 1
-        n = n // 2
-        return self.count_one(n,count)
+    def dp(self, n):
+        count = 0
+        if n not in self.memo:
+            if n % 2 == 1:
+                count = 1
+            num = n >> 1
+            self.memo[n] = count + self.dp(num)
+
+        return self.memo[n]
+    
 
     def countBits(self, n: int) -> List[int]:
+        self.memo = {}
         res = []
+
+        self.memo[0] = 0
+        self.memo[1] = 1
+
         for i in range(0,n+1):
-            count = self.count_one(i, 0)
-            res.append(count)
-            
+            ans = self.dp(i)
+            res.append(ans)
+
         return res
