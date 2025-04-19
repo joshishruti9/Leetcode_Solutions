98a96656b8f0e48fc044f13cb4349a8eed3bd9a4
Shruti Joshi <joshishruti009@gmail.com>
2025-04-19 14:20:32 -0700

    File updated at 4/19/2025, 2:20:31 PM

--- 111-MinimumDepthofBinaryTree/111-MinimumDepthofBinaryTree.py
+++ 111-MinimumDepthofBinaryTree/111-MinimumDepthofBinaryTree.py
@@ -1,28 +1,45 @@
-# Last updated: 4/19/2025, 1:56:46 PM
+# Last updated: 4/19/2025, 2:20:31 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    def traverse(self, node, curr_length):
-
-        if node.left is None and node.right is None:
-            self.min_length = min(self.min_length, curr_length)
-            return
-
-        if node.left:
-            self.traverse(node.left, curr_length + 1)
-        if node.right:
-            self.traverse(node.right, curr_length + 1)
-
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
+
+        if root is None:
             return 0
 
-        self.min_length = float("inf")
-        self.traverse(root, 1)
-        
-        return self.min_length
+        queue = []
+        curr_length = 1
+        queue.append(root)
+
+        while queue:
+            queue2 = []
+            
+            for node in queue:
+                left_flag = right_flag = False
+                if node is None:
+                    continue
+
+                print(node.val)
+
+                if node.left:
+                    queue2.append(node.left)
+                else:
+                    left_flag = True
+
+                if node.right:
+                    queue2.append(node.right)
+                else:
+                    right_flag = True
+                
+                if right_flag and left_flag:
+                    print(curr_length," ",node.val)
+                    return curr_length
+
+            queue = queue2 
+            curr_length += 1
+
+        return curr_length      
