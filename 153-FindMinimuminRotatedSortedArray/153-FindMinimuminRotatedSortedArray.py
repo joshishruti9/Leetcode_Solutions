c0c26d16c17d57709ed90c2f2aa02053c7fa34e3
Shruti Joshi <joshishruti009@gmail.com>
2025-05-05 16:06:50 -0700

    File updated at 5/5/2025, 4:06:50 PM

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.py
@@ -1,9 +1,21 @@
-# Last updated: 5/5/2025, 3:20:53 PM
+# Last updated: 5/5/2025, 4:06:50 PM
 class Solution:
+    def bisect_left(self, nums, low, high):
+
+        while low < high:
+            mid = (low+high) // 2
+            if nums[mid] < nums[mid+1] and nums[mid] < nums[high]:
+                high = mid
+            else:
+                low = mid+1
+        
+        return low
+
     def findMin(self, nums: List[int]) -> int:
         n = len(nums)
+        index = self.bisect_left(nums, 0, n-1)
 
-        output = min(nums)
-
-        return output
-        
+        if index == 0 and nums[index] > nums[n-1]:
+            return nums[n-1]
+     
+        return nums[index]
