e1a3e14e9e151e38568f2ecb22a043de4f23dc83
Shruti Joshi <joshishruti009@gmail.com>
2025-05-07 13:16:38 -0700

    File updated at 5/7/2025, 1:16:36 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,15 +1,25 @@
-# Last updated: 5/7/2025, 12:41:46 PM
+# Last updated: 5/7/2025, 1:16:36 PM
+from heapq import heappush, heappop
 from collections import Counter
 
-def sort_func(item):
-    return item[1]
-
 class Solution:
-
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
         counter = Counter(nums)
 
-        sorted_list = sorted(counter.items(), key = sort_func, reverse = True)
+        min_heap = []
 
-        return [item[0] for item in sorted_list[:k]]
+        for key, value in counter.items():
+            if len(min_heap) < k:
+                heappush(min_heap,(value,key))
+                print(min_heap)
+            else:
+                v, keys = heappop(min_heap)
+                print(v," ",keys)
+                print(value," ",key)
+                if value > v:
+                    heappush(min_heap,(value,key))
+                else:
+                    heappush(min_heap,(v,keys))
+                print(min_heap) 
+
+        return [item[1] for item in min_heap]   
