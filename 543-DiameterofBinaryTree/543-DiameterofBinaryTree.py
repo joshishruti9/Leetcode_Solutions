784e24f5ccc432edf2157dc8768912806ecc199d
Shruti Joshi <joshishruti009@gmail.com>
2025-05-09 11:28:32 -0700

    File updated at 5/9/2025, 11:28:33 AM

--- 543-DiameterofBinaryTree/543-DiameterofBinaryTree.py
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 5/5/2025, 10:27:02 PM
+# Last updated: 5/9/2025, 11:28:33 AM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,26 +7,25 @@
 #         self.right = right
 class Solution:
     def traverse(self, node):
-        
         if node.left is None and node.right is None:
             return 1
-
-        l_len = r_len = 0
-
-        if node.left:
-            l_len = self.traverse(node.left)
-        if node.right:
-            r_len = self.traverse(node.right)
-
-        self.max = max(self.max, (l_len + r_len))
         
-        return max(r_len, l_len) + 1
-       
+        l_depth = r_depth = 0
+        if node.left:
+            l_depth = self.traverse(node.left)
+        if node.right:
+            r_depth = self.traverse(node.right)
+
+        self.max = max(self.max, (l_depth+r_depth))
+
+        return max(l_depth,r_depth)+1
 
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
+        
         self.max = 0
-        max_len = self.traverse(root)
+        self.traverse(root)
 
         return self.max
+        
