a33672be718599d252fcb0864260d7759be65999
Shruti Joshi <joshishruti009@gmail.com>
2025-05-09 22:50:40 -0700

    File updated at 5/9/2025, 10:50:40 PM

--- 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.py
+++ 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.py
@@ -1,4 +1,4 @@
-# Last updated: 5/9/2025, 10:50:19 PM
+# Last updated: 5/9/2025, 10:50:40 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,7 +7,6 @@
 #         self.right = right
 class Solution:
     def traverse(self, node):
-        print("self.max:", self.max)
         if node.left is None and node.right is None:
             self.max = max(self.max, node.val)
             return node.val
@@ -24,9 +23,7 @@ class Solution:
         r_total = rval + node.val
 
         self.max = max(self.max, total_val, node.val, l_total, r_total)
-        print("self.max:", self.max)
         curr_val = max(l_total, r_total, node.val)
-        print("curr_val:", curr_val)
 
         return curr_val
 
