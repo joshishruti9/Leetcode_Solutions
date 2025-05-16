8bd3f6f54bf9d8ca31adbf15ae4228cbbfabccd0
Shruti Joshi <joshishruti009@gmail.com>
2025-05-15 22:02:45 -0700

    Create file

---
+++ 1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.py
@@ -0,0 +1,34 @@
+# Last updated: 5/15/2025, 10:02:46 PM
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def convert_binary(self, num):
+        count = 0
+        bin_num = 0
+
+        while num > 0:
+            digit = num % 10
+            bin_num = digit * (2 ** count) + bin_num
+            num = num//10
+            count += 1
+
+        return bin_num
+
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+
+        node = head
+        num = 0
+        count = 0
+
+        while node:
+            num = (num * 10) + node.val
+            node = node.next
+            count += 1
+        
+        print(num)
+        bin_num = self.convert_binary(num)
+        return bin_num
