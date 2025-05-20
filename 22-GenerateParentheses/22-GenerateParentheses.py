875726aac52bd7089c23b2cd9f0e1f415415d85a
Shruti Joshi <joshishruti009@gmail.com>
2025-05-19 20:00:55 -0700

    Create file

---
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -0,0 +1,22 @@
+# Last updated: 5/19/2025, 8:00:55 PM
+class Solution:
+    def traverse(self, open_count, close_count, n, curr_res):
+        if close_count == n and open_count == n:
+            self.output.append("".join(curr_res[:])) 
+            return
+        
+        if open_count < n:
+            curr_res.append('(')
+            self.traverse(open_count+1, close_count, n, curr_res)
+            curr_res.pop()
+        
+        if close_count < open_count:
+            curr_res.append(')')
+            self.traverse(open_count, close_count+1, n, curr_res)
+            curr_res.pop()  
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.output = []
+        self.traverse(0,0,n,[])
+
+        return self.output
