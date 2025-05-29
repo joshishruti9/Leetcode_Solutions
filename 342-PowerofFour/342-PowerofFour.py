7a8867a33959b7ec49a11543dc8e243e2b73fc80
Shruti Joshi <joshishruti009@gmail.com>
2025-05-28 19:24:23 -0700

    File updated at 5/28/2025, 7:24:23 PM

--- 342-PowerofFour/342-PowerofFour.py
+++ 342-PowerofFour/342-PowerofFour.py
@@ -1,15 +1,19 @@
-# Last updated: 5/28/2025, 7:16:51 PM
+# Last updated: 5/28/2025, 7:24:23 PM
+import math
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n == 1:
-            return True
 
-        for i in range(n//2):
+        if n <= 0:
+            return False
+            
+        log_val = math.log(n, 4)        
+        return int(log_val) == log_val
+
+        '''for i in range(n//2):
             num = (4 ** i)
             if n == num:
                 return True
             elif num > n:
                 break
         
-        return False
-        
+        return False'''
