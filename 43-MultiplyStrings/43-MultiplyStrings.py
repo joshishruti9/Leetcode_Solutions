814e2c7da9061e45d566229fdcbab2589c3f3277
Shruti Joshi <joshishruti009@gmail.com>
2025-05-29 16:56:57 -0700

    File updated at 5/29/2025, 4:56:58 PM

--- 43-MultiplyStrings/43-MultiplyStrings.py
+++ 43-MultiplyStrings/43-MultiplyStrings.py
@@ -1,8 +1,27 @@
-# Last updated: 5/29/2025, 4:41:43 PM
+# Last updated: 5/29/2025, 4:56:58 PM
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
 
-        return str(int(num1) * int(num2))
+        n1 = len(num1) - 1
+        n2 = len(num2) - 1
+        ans = 0
+        digit = 0
 
+        for i in range(n1,-1,-1):
+            carry = 0
+            count = 0
+            mul = 0
+            for j in range(n2, -1, -1):
+                num = int(num2[j]) * int(num1[i]) % 10
+                mul += ((10 ** count) * (num + carry))
+                carry = int(num2[j]) * int(num1[i]) // 10
+                count += 1
+            
+            if carry > 0:
+                mul = carry * (10 ** count) + mul
+            
+            mul = mul * (10 ** digit)
+            ans += mul
+            digit += 1
 
-        
+        return str(ans)
