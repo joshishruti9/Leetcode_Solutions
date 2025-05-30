7087d5afc597ca79b9c27a0ea3aa46741c46975a
Shruti Joshi <joshishruti009@gmail.com>
2025-05-30 13:20:46 -0700

    File updated at 5/30/2025, 1:20:47 PM

--- 74-Searcha2DMatrix/74-Searcha2DMatrix.py
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.py
@@ -1,14 +1,29 @@
-# Last updated: 5/30/2025, 12:57:36 PM
+# Last updated: 5/30/2025, 1:20:47 PM
 class Solution:
+    def bisect_left(self, arr, low, high, target):
+
+        while low < high:
+            mid = (low + high) // 2
+            if arr[mid] < target:
+                low = mid + 1
+            else:
+                high = mid
+        
+        return low
+
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
 
         m = len(matrix)
         n = len(matrix[0])
+        count = 0
 
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j] == target:
+        for row in matrix:
+            if target >= row[0] and target <= row[n-1]:
+                index = self.bisect_left(row, 0, n, target)
+                if matrix[count][index] == target:
                     return True
+                else:
+                    return False
+            count += 1
         
         return False
-        
