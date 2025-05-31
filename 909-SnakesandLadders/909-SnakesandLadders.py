caed63200bd27d2152258c524adc780ab210b2ce
Shruti Joshi <joshishruti009@gmail.com>
2025-05-31 13:00:55 -0700

    File updated at 5/31/2025, 1:00:55 PM

--- 909-SnakesandLadders/909-SnakesandLadders.py
+++ 909-SnakesandLadders/909-SnakesandLadders.py
@@ -1,49 +1,63 @@
-# Last updated: 4/3/2025, 11:11:04 PM
+# Last updated: 5/31/2025, 1:00:55 PM
 from collections import deque
-from math import ceil
-
 class Solution:
-    def get_row_col(self, n, value):
-        pos_ceil = ceil(value / n)
-        r = n - pos_ceil
-        if (pos_ceil % 2) == 1:
-            c = (value - 1) % n
+    def get_row_col(self, pos, n):
+        row =  (pos-1) // n
+
+        col = self.get_col(pos, row, n)
+
+        return (n - row - 1, col)
+    
+    def get_col(self, pos, row, n):
+
+        if row % 2 == 0:
+            if pos % n == 0:
+                col = n - 1
+            else:
+                col = (pos % n) - 1
         else:
-            c = pos_ceil * n - value
-        return r,c
+            if pos % n == 0:
+                col = 0
+            else:
+                col = n - (pos % n)
+        return col
 
-         
-    def traverse(self, board, start, end, n, queue, visited):
+    def traverse(self, board, queue, visited, n):
+
         while queue:
-            node, steps = queue.popleft()
-            if node == end:
-                return steps
+            curr_pos, step_count = queue.popleft()
+            
+            if curr_pos == (n * n):
+                return step_count
 
-            for i in range(1,7):
-                pos = node + i
-                if pos > (n * n):
+            for dice in range(1, 7):
+                next_pos = curr_pos + dice
+
+                if next_pos > (n * n):
                     break
-                r,c = self.get_row_col(n,pos)
-                if board[r][c] != -1:
-                    pos = board[r][c]
-                    r, c = self.get_row_col(n, pos)
-                    if pos not in visited:
-                        visited.add(pos)
-                        queue.append((pos, steps+1))
+
+                
+                #get row col for dice_val
+                row, col = self.get_row_col(next_pos, n)
+
+                if board[row][col] != -1:
+                    if board[row][col] not in visited:
+                        dest = board[row][col]
+                        queue.append((dest, step_count+1))
+                        visited.add(dest)
                 else:
-                    if pos not in visited:
-                        visited.add(pos)
-                        queue.append((pos, steps+1))
-        return -1
+                    if next_pos not in visited:
+                        queue.append((next_pos, step_count+1))
+                        visited.add(next_pos)
+
+        return -1                 
 
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        visited = set()
         queue = deque()
-
+        visited = set()
         n = len(board)
 
-        start = 1
-        end = n * n
-        queue.append((1,0))
         visited.add(1)
-        return self.traverse(board,start, end, n, queue, visited)
+        queue.append((1,0))
+       
+        return self.traverse(board, queue, visited, n)
