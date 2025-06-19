bf07c0d7ac7fb12e77c2eb63453a6ebcda69e861
Shruti Joshi <joshishruti009@gmail.com>
2025-06-18 21:20:39 -0700

    File updated at 6/18/2025, 9:20:38 PM

--- 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.py
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.py
@@ -1,4 +1,4 @@
-# Last updated: 5/16/2025, 3:00:38 PM
+# Last updated: 6/18/2025, 9:20:38 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,20 +6,21 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        length = 0
+        if not head:
+            return None
+        n = 0
 
         node = head
-
         while node:
-            length += 1
+            n += 1
             node = node.next
-
+        
         count = 0
         node = head
-        while node:
-            if count == (length // 2):
-                return node
-            
+
+        while count != n//2:
             count += 1
             node = node.next
-
+        
+        return node
+        
