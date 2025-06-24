c52a68bc22f47e7562fa2bb84bfdf552452af2fc
Shruti Joshi <joshishruti009@gmail.com>
2025-06-23 23:05:08 -0700

    File updated at 6/23/2025, 11:05:08 PM

--- 1470-ShuffletheArray/1470-ShuffletheArray.py
+++ 1470-ShuffletheArray/1470-ShuffletheArray.py
@@ -1,16 +1,17 @@
-# Last updated: 5/24/2025, 8:23:26 PM
+# Last updated: 6/23/2025, 11:05:08 PM
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
 
         i = 0
         j = n
+
         res = []
 
-        while j < len(nums) and i < j:
+        while j < len(nums):
             res.append(nums[i])
             res.append(nums[j])
 
             i += 1
             j += 1
-        
+
         return res
