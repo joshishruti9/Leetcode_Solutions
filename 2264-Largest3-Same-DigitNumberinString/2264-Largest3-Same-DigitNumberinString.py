0e661927dc82589c96db13e72d7d97f95e7dd63e
Shruti Joshi <joshishruti009@gmail.com>
2025-06-25 22:46:01 -0700

    File updated at 6/25/2025, 10:46:01 PM

--- 2264-Largest3-Same-DigitNumberinString/2264-Largest3-Same-DigitNumberinString.py
+++ 2264-Largest3-Same-DigitNumberinString/2264-Largest3-Same-DigitNumberinString.py
@@ -1,4 +1,4 @@
-# Last updated: 6/25/2025, 10:42:24 PM
+# Last updated: 6/25/2025, 10:46:01 PM
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
 
@@ -7,6 +7,9 @@ class Solution:
 
         while i < len(num)-2:
             if num[i] == num[i+1] and num[i+1] == num[i+2]:
-                max_val = max(max_val, num[i]) 
-            i += 1
+                max_val = max(max_val, num[i])
+                i += 3  
+            else:
+                i += 1
+
         return  max_val*3 if max_val else ""
