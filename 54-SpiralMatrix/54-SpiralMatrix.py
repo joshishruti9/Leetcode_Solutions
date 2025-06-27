6cd28c67f1ba5067c370259889d38d88c443fcb0
Shruti Joshi <joshishruti009@gmail.com>
2025-06-27 01:16:37 -0700

    File updated at 6/27/2025, 1:16:37 AM

--- 54-SpiralMatrix/54-SpiralMatrix.py
+++ 54-SpiralMatrix/54-SpiralMatrix.py
@@ -1,51 +1,38 @@
-# Last updated: 3/23/2025, 2:06:36 PM
-import math
+# Last updated: 6/27/2025, 1:16:37 AM
 class Solution:
-    def traverse_spiral(self, spiral_count, row_nums, col_nums, output, matrix):
-        sr = sc = spiral_count
-        er = row_nums - spiral_count - 1 # excluding
-        ec = col_nums - spiral_count - 1 # excluding
-
-        if sr == er and sc == ec:
-            output.append(matrix[sr][sc])
-            return
+    def traverse(self, n, start, mat):
+        sr = sc = start
+        er = ec = n - 1 - start
 
         if sr == er:
-            for j in range(sc,ec+1):
-                output.append(matrix[sr][j])
-            return
-        
-        if sc == ec:
-            for i in range(sr,er+1):
-                output.append(matrix[i][sc])
-            return
+            mat[sr][er] = self.num
+            return mat
 
-        for j in range(sc, ec):
-            output.append(matrix[sr][j])
-
-        for i in range(sr, er):
-            output.append(matrix[i][ec])
+        for j in range(sc,ec):
+            mat[sr][j] = self.num
+            self.num += 1
         
-        for j in range(ec,sc,-1):
-            output.append(matrix[er][j])
+        for i in range(sr,er):
+            mat[i][ec] = self.num
+            self.num += 1
+        
+        for j in range(ec, sc,-1):
+            mat[er][j] = self.num
+            self.num += 1
         
         for i in range(er,sr,-1):
-            output.append(matrix[i][sc]) 
-
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-
-        row_nums = len(matrix)
-        col_nums = len(matrix[0])
-        output = []
-
-        print(row_nums, " ",col_nums)
-
-        spiral_count = min(row_nums, col_nums) / 2
-        spiral_count = math.ceil(spiral_count)
-
-        for i in range(spiral_count):
-            self.traverse_spiral(i,row_nums,col_nums,output,matrix)
-
-        return output
-
+            mat[i][sc] = self.num
+            self.num += 1
         
+
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        spiral_count = math.ceil(n / 2)
+        self.num = 1
+
+        mat = [[0] * n for i in range(n)]
+
+        #calculate spiral count
+        for i in range(spiral_count):
+            self.traverse(n, i, mat)
+
+        return mat
