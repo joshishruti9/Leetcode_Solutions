3f819fd6cbdfe89551444d0c85068ec660ce470e
Shruti Joshi <joshishruti009@gmail.com>
2025-06-30 21:57:11 -0700

    File updated at 6/30/2025, 9:57:11 PM

--- 88-MergeSortedArray/88-MergeSortedArray.py
+++ 88-MergeSortedArray/88-MergeSortedArray.py
@@ -1,27 +1,30 @@
-# Last updated: 6/30/2025, 9:55:59 PM
+# Last updated: 6/30/2025, 9:57:11 PM
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        k = len(nums1) - 1
-        i = m - 1
-        j = n - 1
+        if n == 0:
+            return
+        if m == 0:
+            nums1[0:n] = nums2
 
-        while i >= 0 and j >= 0:
-            if nums1[i] >= nums2[j]:
-                nums1[k] = nums1[i]
-                k -= 1
-                i -= 1
+        ptr1_tail = m - 1
+        ptr2_tail = n - 1
+        ptr = m + n - 1
+
+        while ptr1_tail >= 0 and ptr2_tail >= 0:
+            if nums1[ptr1_tail] > nums2[ptr2_tail]:
+                nums1[ptr] = nums1[ptr1_tail]
+                ptr1_tail -= 1
             else:
-                nums1[k] = nums2[j]
-                k -= 1
-                j -= 1
+                nums1[ptr] = nums2[ptr2_tail]
+                ptr2_tail -= 1
+            ptr -= 1
+            
         
-        index = i if i > 0 else j
-        nums = nums1 if i > 0 else nums2
+        while ptr2_tail >= 0:
+            nums1[ptr] = nums2[ptr2_tail]
+            ptr2_tail -= 1
+            ptr -= 1
 
-        while index >= 0:
-            nums1[k] = nums[index]
-            index -= 1
-            k -= 1        
