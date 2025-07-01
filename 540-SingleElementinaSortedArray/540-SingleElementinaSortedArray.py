d6abfcf5c2a4511e1b47a67102b0cd6a6e1a6dde
Shruti Joshi <joshishruti009@gmail.com>
2025-06-30 20:42:24 -0700

    File updated at 6/30/2025, 8:42:25 PM

--- 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.py
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.py
@@ -1,14 +1,26 @@
-# Last updated: 6/27/2025, 1:32:32 AM
+# Last updated: 6/30/2025, 8:42:25 PM
 class Solution:
+    def binary(self, nums, low, high):
+
+        while low < high:
+            mid = (low + high) // 2
+            if mid % 2 == 0:
+                if nums[mid] != nums[mid+1]:
+                    high = mid
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] != nums[mid+1]:
+                    low = mid + 1
+                else:
+                    high = mid
+                
+        return nums[low]
+
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        output = 0
-        i = 0
         n = len(nums)
 
-        while i < (n-2):
-            if nums[i] == nums[i+1]:
-                i += 2
-            else:
-                return nums[i]
-        
-        return nums[n-1]
+        if len(nums) == 1:
+            return nums[0]
+
+        return self.binary(nums, 0, n-1)
