5bff67ea4a4504dcdee835cf79eba81b63a3ba95
Shruti Joshi <joshishruti009@gmail.com>
2025-07-06 18:40:39 -0700

    File updated at 7/6/2025, 6:40:40 PM

--- 845-LongestMountaininArray/845-LongestMountaininArray.py
+++ 845-LongestMountaininArray/845-LongestMountaininArray.py
@@ -1,4 +1,4 @@
-# Last updated: 7/6/2025, 6:40:02 PM
+# Last updated: 7/6/2025, 6:40:40 PM
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
         max_length = 0
@@ -12,24 +12,19 @@ class Solution:
                 i += 1
  
             start = i
-            print("start ",start)
-
+            
             while i < n-1 and arr[i] < arr[i+1]:
                 i += 1
             
             peak = i
-            print("peak ",peak)
 
             while i < n-1 and arr[i] > arr[i+1]:
                 i += 1
 
             end = i
-            print("start ",start)
-            print("end ",end)
-            
+
             if peak != end:
                 max_length = max(max_length, (end-start+1))
-                print("end ",max_length)
             
         return max_length
         
