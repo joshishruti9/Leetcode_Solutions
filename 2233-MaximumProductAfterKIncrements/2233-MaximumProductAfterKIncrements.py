86c9c1abaadc5780cb5270b7369c1592fdc580f1
Shruti Joshi <joshishruti009@gmail.com>
2025-07-09 17:41:44 -0700

    Create 2233-MaximumProductAfterKIncrements

---
+++ 2233-MaximumProductAfterKIncrements/2233-MaximumProductAfterKIncrements.py
@@ -0,0 +1,20 @@
+# Last updated: 7/9/2025, 5:41:44 PM
+from heapq import heappush, heappop
+class Solution:
+    def maximumProduct(self, nums: List[int], k: int) -> int:
+        min_heap = []
+
+        for num in nums:
+            heappush(min_heap, num)
+        
+        while k > 0:
+            num = heappop(min_heap)
+            heappush(min_heap, num+1)
+            k -= 1
+        
+        mul = 1
+
+        while min_heap:
+            mul = mul * heappop(min_heap)
+        
+        return mul % (10 ** 9 + 7)
