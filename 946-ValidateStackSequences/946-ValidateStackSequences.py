8f4a50fb776a76c77983a510c27d32639dc58db8
Shruti Joshi <joshishruti009@gmail.com>
2025-07-09 17:01:07 -0700

    Create 946-ValidateStackSequences

---
+++ 946-ValidateStackSequences/946-ValidateStackSequences.py
@@ -0,0 +1,24 @@
+# Last updated: 7/9/2025, 5:01:06 PM
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+
+        stack = []
+        i = 0
+        j = 0
+
+        while j < len(pushed) and i < len(popped):
+            if stack and popped[i] == stack[-1]:
+                stack.pop()
+                i += 1
+            else:
+                stack.append(pushed[j])
+                j += 1
+
+        while stack and i < len(popped):
+            if popped[i] == stack[-1]:
+                stack.pop()
+                i += 1
+            else:
+                break
+
+        return len(stack) == 0
