d306cc032d7197c774280cc756bcbc760785e574
Shruti Joshi <joshishruti009@gmail.com>
2025-07-23 11:48:34 -0700

    File updated at 7/23/2025, 11:48:34 AM

--- 228-SummaryRanges/228-SummaryRanges.py
+++ 228-SummaryRanges/228-SummaryRanges.py
@@ -1,28 +1,31 @@
-# Last updated: 4/13/2025, 3:29:34 PM
+# Last updated: 7/23/2025, 11:48:34 AM
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
+
         start = 0
         end = 0
         res = []
 
         if len(nums) == 0:
-            return []
-        
+            return res
+
         while end < len(nums)-1:
-            if nums[end] + 1 == nums[end+1]:
+
+            if nums[end] == nums[end+1]-1:
                 end += 1
             else:
-                if start != end:
-                    res.append(str(nums[start]) + "->" + str(nums[end]))
-                    end += 1
-                    start = end
-                else:
+                if start == end:
                     res.append(str(nums[start]))
                     start += 1
                     end += 1
-        if start != end:
-            res.append(str(nums[start]) + "->" + str(nums[end]))
-        else:
+                else:
+                    res.append(str(nums[start]) + "->" + str(nums[end]))
+                    end += 1
+                    start = end
+
+        if start == end:
             res.append(str(nums[start]))
-                    
+        else:
+            res.append(str(nums[start]) + "->" + str(nums[end]))
+
         return res
