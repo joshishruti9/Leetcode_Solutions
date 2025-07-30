59c005f0e26479e24fef05e574cf7aad157b0202
Shruti Joshi <joshishruti009@gmail.com>
2025-07-30 16:21:34 -0700

    File updated at 7/30/2025, 4:21:34 PM

--- 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.py
+++ 236-LowestCommonAncestorofaBinaryTree/236-LowestCommonAncestorofaBinaryTree.py
@@ -1,3 +1,4 @@
+# Last updated: 7/30/2025, 4:21:34 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -6,30 +7,34 @@
 #         self.right = None
 
 class Solution:
-    def traverse(self,node,p,q):
+    def traverse(self, node, p, q):
+        
 
-        rnode = pnode = qnode = False
+        l_flag = r_flag = flag = False
 
         if node.val == p.val or node.val == q.val:
-            rnode = True
-
+            flag = True
+        
         if node.left is None and node.right is None:
-            return rnode
-
+            return flag
+        
+        
         if node.left:
-            pnode = self.traverse(node.left,p,q)
+            l_flag = self.traverse(node.left, p, q)
         if node.right:
-            qnode = self.traverse(node.right,p,q)
+            r_flag = self.traverse(node.right, p, q)
 
-        if (pnode and qnode) or (pnode and rnode) or (qnode and rnode):
+        if (l_flag and r_flag) or (r_flag and flag) or (l_flag and flag):
             self.lca = node
         
-        return pnode or qnode or rnode
-        
-        
+        return l_flag or r_flag or flag
+
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
         if root is None:
             return None
+
         self.lca = None
-        self.traverse(root,p,q)
-        return self.lca      
+        self.traverse(root, p, q)
+        
+        return self.lca
