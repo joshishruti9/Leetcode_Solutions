e9850e3295f45a042fe07c9db8db52a5d3a62d27
Shruti Joshi <joshishruti009@gmail.com>
2025-07-30 15:44:10 -0700

    File updated at 7/30/2025, 3:44:10 PM

--- 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.py
+++ 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.py
@@ -1,4 +1,4 @@
-# Last updated: 5/11/2025, 6:29:19 PM
+# Last updated: 7/30/2025, 3:44:10 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,35 +7,36 @@
 #         self.right = right
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-
-        if root is None:
-            return []
         
-        queue = []
-        queue.append(root)
-        queue2 = []
-        level = 1
         res = []
 
+        if root is None:
+            return res
+        
+        level = 1
+        queue = [root]
 
-        while queue:  
-            queue2 = []
+        while queue:
             output = []
+            queue2 = []
+
             for node in queue:
                 if node is None:
                     continue
                 
                 output.append(node.val)
+
                 if node.left:
                     queue2.append(node.left)
                 if node.right:
                     queue2.append(node.right)
-                    
-            if level%2 != 0:
-                res.append(output)
-            else:
+            
+            if level % 2 == 0:
                 res.append(output[::-1])
+            else:
+                res.append(output)
+            
             level += 1
             queue = queue2
-        
+    
         return res
