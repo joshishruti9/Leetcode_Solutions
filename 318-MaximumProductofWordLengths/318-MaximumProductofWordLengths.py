299c87b92c7e357cebf1a3facfb7366af3a60cce
Shruti Joshi <joshishruti009@gmail.com>
2025-08-06 14:53:20 -0700

    File updated at 8/6/2025, 2:53:22 PM

--- 318-MaximumProductofWordLengths/318-MaximumProductofWordLengths.py
+++ 318-MaximumProductofWordLengths/318-MaximumProductofWordLengths.py
@@ -1,9 +1,12 @@
-# Last updated: 8/5/2025, 6:11:31 PM
-class Solution(object):
-    def maxProduct(self, words):
-        bitmasks = [sum(1 << (ord(c) - ord('a')) for c in set(w)) for w in words]
-        products = [len(words[i]) * len(words[j]) 
-                    for i in range(len(words)) 
-                    for j in range(i + 1, len(words)) 
-                    if not bitmasks[i] & bitmasks[j]]
-        return max(products) if products else 0
+# Last updated: 8/6/2025, 2:53:22 PM
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        max_len = 0
+    
+        for i in range(len(words)):
+            for j in range(i+1, len(words)):
+                if not (set(words[i])).intersection(set(words[j])):
+                    prod_len = len(words[i]) * len(words[j])
+                    max_len = max(max_len, prod_len)
+
+        return max_len
