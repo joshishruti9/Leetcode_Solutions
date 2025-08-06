3642a9a292d85bc9e9594db33e99504c6bd7db2a
Shruti Joshi <joshishruti009@gmail.com>
2025-08-06 14:59:39 -0700

    File updated at 8/6/2025, 2:59:42 PM

--- 1466-ReorderRoutestoMakeAllPathsLeadtotheCityZero/1466-ReorderRoutestoMakeAllPathsLeadtotheCityZero.py
+++ 1466-ReorderRoutestoMakeAllPathsLeadtotheCityZero/1466-ReorderRoutestoMakeAllPathsLeadtotheCityZero.py
@@ -1,26 +1,45 @@
-# Last updated: 7/6/2025, 2:43:49 PM
-from collections import defaultdict
-
+# Last updated: 8/6/2025, 2:59:42 PM
+from collections import deque
 class Solution:
-    def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        roads = set()
-        seen = {0}
+    def reorder_count(self, adj_list):
+        visited = set()
+        queue = deque() 
+        queue.append(0)
+        visited.add(0)
+        count = 0
 
-        graph = defaultdict(list)
+        while queue:
+            node = queue.popleft()
+            node_dict = adj_list.get(node, {})
+            for out in node_dict.get('out', []):
+                if out not in visited:
+                    count += 1
+                    queue.append(out)
+                    visited.add(out)
+            
+            for innode in node_dict.get("in",[]):
+                if innode not in visited:
+                    queue.append(innode)
+                    visited.add(innode)
+                
+        return count
 
-        for x, y in connections:
-            graph[x].append(y)
-            graph[y].append(x)
-            roads.add((x,y))
+
+    def create_adjList(self, connections, n):
+        adj_list = dict()
+        for u, v in connections:
+            if u not in adj_list: adj_list[u] = {'out': [], 'in': []}
+            if v not in adj_list: adj_list[v] = {'out': [], 'in': []}
+
+            adj_list[u]["out"].append(v)
+            adj_list[v]["in"].append(u)
         
-        def dfs(node):
-            ans = 0
-            for n in graph[node]:
-                if n not in seen:
-                    seen.add(n)
-                    if (node, n) in roads:
-                        ans+=1
-                    ans+=dfs(n)
-            return ans
+        return adj_list
 
-        return dfs(0)
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+
+        adj_list = self.create_adjList(connections,n)
+        
+        count = self.reorder_count(adj_list)
+
+        return count
