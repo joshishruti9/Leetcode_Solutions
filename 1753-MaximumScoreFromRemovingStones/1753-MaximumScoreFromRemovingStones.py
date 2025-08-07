33c0dfd1ba362be99b0f75b1de01cfb4c684d562
Shruti Joshi <joshishruti009@gmail.com>
2025-08-07 12:04:21 -0700

    File updated at 8/7/2025, 12:04:22 PM

--- 1753-MaximumScoreFromRemovingStones/1753-MaximumScoreFromRemovingStones.py
+++ 1753-MaximumScoreFromRemovingStones/1753-MaximumScoreFromRemovingStones.py
@@ -1,4 +1,4 @@
-# Last updated: 8/6/2025, 1:13:16 PM
+# Last updated: 8/7/2025, 12:04:22 PM
 from heapq import heappush, heappop
 class Solution:
     def maximumScore(self, a: int, b: int, c: int) -> int:
@@ -13,8 +13,6 @@ class Solution:
         while True:
             max1 = -heappop(max_heap)
             max2 = -heappop(max_heap)
-
-            print(max1, " ", max2)
 
             if max1 == 0 or max2 == 0:
                 break
