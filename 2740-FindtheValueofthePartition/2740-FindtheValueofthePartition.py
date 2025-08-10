08963e7f07c61df9e39c25a1d8f881f14518bccf
Shruti Joshi <joshishruti009@gmail.com>
2025-08-09 22:29:33 -0700

    File updated at 8/9/2025, 10:29:33 PM

--- 2740-FindtheValueofthePartition/2740-FindtheValueofthePartition.py
+++ 2740-FindtheValueofthePartition/2740-FindtheValueofthePartition.py
@@ -1,9 +1,9 @@
-# Last updated: 8/9/2025, 10:28:50 PM
+# Last updated: 8/9/2025, 10:29:33 PM
 class Solution:
     def findValueOfPartition(self, nums: List[int]) -> int:
 
         nums.sort()
-        min_diff = float("inf")
+        min_diff = 1e9
 
         for i in range(1, len(nums)):
             min_diff = min(min_diff, nums[i] - nums[i-1])
