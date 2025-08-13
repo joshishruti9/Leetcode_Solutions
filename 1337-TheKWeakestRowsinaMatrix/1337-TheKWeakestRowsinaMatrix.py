183b1a5bd384ba7e4306013929cc7b50a4abc2a6
Shruti Joshi <joshishruti009@gmail.com>
2025-08-13 14:53:52 -0700

    Create 1337-TheKWeakestRowsinaMatrix

---
+++ 1337-TheKWeakestRowsinaMatrix/1337-TheKWeakestRowsinaMatrix.py
@@ -0,0 +1,26 @@
+# Last updated: 8/13/2025, 2:53:52 PM
+from heapq import heappush, heappop
+
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        arr = []
+        res = []
+        m = len(mat)
+
+        for i, row in enumerate(mat):
+            count = sum(row)
+            if len(arr) < k:
+                heappush(arr, (-count, -i))
+            else:
+                count1, j = heappop(arr)
+                print(count," ",i," ",count1 * -1," ",j)
+                if count < -count1 or (count == -count1 and i < j) :
+                    heappush(arr,(-count,-i))
+                else:
+                    heappush(arr,(count1,j))
+
+        while arr:
+            count, i = heappop(arr)
+            res.append(-i)
+        
+        return res[::-1]
