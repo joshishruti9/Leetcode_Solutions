69ddbf9b65e3a8a7a623c6c6c279c8ffa509e28d
Shruti Joshi <joshishruti009@gmail.com>
2025-08-15 15:12:41 -0700

    File updated at 8/15/2025, 3:12:42 PM

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,32 +1,39 @@
-# Last updated: 8/15/2025, 2:56:23 PM
-from typing import List
-
+# Last updated: 8/15/2025, 3:12:42 PM
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
-
         stack = []
         total = 0
 
         i = 0
+        while i< len(height) and height[i] <= 0:
+            i += 1
+        
+        if i == len(height):
+            return 0
+
         curr_high = height[i]
 
         while i < len(height):
-            if height[i] < curr_high:
+            if height[i] <= curr_high:
                 stack.append(height[i])
+                i += 1
             else:
+                min_val = min(curr_high, height[i])
+                max_val = max(curr_high, height[i])
                 while stack:
-                    total += (curr_high - stack.pop())
+                    val = stack.pop()
+                    total += (min_val - val)
                 curr_high = height[i]
-            i += 1
+                stack.append(height[i])
+                i += 1
 
-        # Handle case where water is trapped using the right boundary
         if stack:
-            right_high = stack.pop()
+            right_max = stack.pop()
             while stack:
-                h = stack.pop()
-                total += max(0, right_high - h)
-                right_high = max(right_high, h)
+                val = stack.pop()
+                if val > right_max:
+                    right_max = val
+                else:
+                    total += (right_max - val)
 
         return total
