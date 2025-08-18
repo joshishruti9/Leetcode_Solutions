4e91700f1ecbdb097fca08a5ebd71a1f0d1aab13
Shruti Joshi <joshishruti009@gmail.com>
2025-08-18 13:26:46 -0700

    File updated at 8/18/2025, 1:26:46 PM

--- 1061-LexicographicallySmallestEquivalentString/1061-LexicographicallySmallestEquivalentString.py
+++ 1061-LexicographicallySmallestEquivalentString/1061-LexicographicallySmallestEquivalentString.py
@@ -1,11 +1,11 @@
-# Last updated: 8/18/2025, 1:25:44 PM
+# Last updated: 8/18/2025, 1:26:46 PM
 class Solution:
     def find(self, root, x):
         if root[x] == x:
             return root[x]
         return self.find(root, root[x])
 
-    def union_find(self, root, rank, x, y):
+    def union_find(self, root, x, y):
 
         root_x = self.find(root, x)
         root_y = self.find(root, y)
@@ -18,34 +18,17 @@ class Solution:
         else:
             root[root_x] = root_y
 
-
-        '''if rank[root_x] > rank[root_y]:
-            root_x < root_y
-            root[root_y] = root_x
-        elif rank[root_y] > rank[root_x]:
-            root[root_x] = root_y
-        else:
-            if x <= y:
-                root[root_y] = root_x
-                rank[root_x] += 1
-            else:
-                root[root_x] = root_y
-                rank[root_y] += 1 '''
-
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
 
         root = {}
-        rank = {}
         res = []
 
         for letter1,letter2 in zip(s1,s2):
             root[letter1] = letter1
             root[letter2] = letter2
-            rank[letter1] = 1
-            rank[letter2] = 1
 
         for letter1, letter2 in zip(s1, s2):
-            self.union_find(root, rank, letter1, letter2) 
+            self.union_find(root, letter1, letter2) 
         
         for letter in baseStr:
             if letter in root:
