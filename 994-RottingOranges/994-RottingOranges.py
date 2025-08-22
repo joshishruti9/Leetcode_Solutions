d27cf4a068463abb1f0163ff19e5249b87e1640b
Shruti Joshi <joshishruti009@gmail.com>
2025-08-22 13:17:21 -0700

    File updated at 8/22/2025, 1:17:22 PM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,28 +1,30 @@
-# Last updated: 3/27/2025, 9:45:56 PM
-from collections import deque
+# Last updated: 8/22/2025, 1:17:22 PM
 class Solution:
-    def traverse(self, visited, m, n, grid, rotten):
+    def find_neighbours(self, queue, visited, m, n, i, j, grid):
+        dirs = [(1,0),(0,1),(-1,0),(0,-1)]
+        for di, dj in dirs:
+            if 0 <= i+di < m and 0 <= j+dj < n and (i+di, j+dj) not in visited:
+                if grid[i+di][j+dj] == 1:
+                    queue.append((i+di,j+dj))
+                    visited.add((i+di, j+dj))
+                    grid[i+di][j+dj] = 2
+
+    def traverse(self, grid, rotten, visited, m, n):
+        queue = []
         count = 0
-        rot_flag = False
-        directions = [(0,1),(1,0),(-1,0),(0,-1)]
         while rotten:
             queue = []
-            for i,j in rotten:
-                for di, dj in directions:
-                    if 0 <= (i+di) < m and 0 <= (j+dj) < n and ((i+di, j+dj)) not in visited and grid[i+di][j+dj] == 1:
-                        grid[i+di][j+dj] = 2
-                        queue.append((i+di,j+dj))
-                        visited.add((i+di,j+dj))
+            for i, j in rotten:
+                self.find_neighbours(queue, visited, m, n, i, j, grid)
             count += 1
-            rotten = queue 
-        return max(count-1,0)
+            rotten = queue
+        return max(0,count - 1)
 
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
+        rotten = []
         visited = set()
-        rotten = deque()
-        count = 0
 
         for i in range(m):
             for j in range(n):
@@ -30,11 +32,12 @@ class Solution:
                     rotten.append((i,j))
                     visited.add((i,j))
 
-        count = self.traverse(visited, m, n, grid, rotten)
+        count = self.traverse(grid, rotten, visited, m, n)
 
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     return -1
-
+        
         return count
+        
