3b59df4c305c6e5d282339d3801b384f6163b89e
Shruti Joshi <joshishruti009@gmail.com>
2026-03-05 13:39:54 -0800

    [169] [Time Beats: 86.14%] [Memory Beats: 81.60%] - LeetPush

--- 545. Boundary of Binary Tree/Majority-Element.py
+++ 545. Boundary of Binary Tree/Majority-Element.py
@@ -1,10 +1,18 @@
 1class Solution:
 2    def majorityElement(self, nums: List[int]) -> int:
 3
-4        num_counter = Counter(nums)
-5        n = len(nums)
+4        count = 0
+5        num = 0
 6
-7        for key, val in num_counter.items():
-8            if val > (n//2):
-9                return key
-10        
+7        for i in range(len(nums)):
+8            if count == 0:
+9                num = nums[i]
+10
+11            if nums[i] == num:
+12                count += 1
+13            else:
+14                count -= 1
+15        
+16        return num
+17            
+18        
