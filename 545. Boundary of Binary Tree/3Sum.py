6e35e0c25b911547195816c553ccde3d89544371
Shruti Joshi <joshishruti009@gmail.com>
2026-03-06 18:11:43 -0800

    [15] [Time Beats: 34.34%] [Memory Beats: 14.19%] - LeetPush

--- 545. Boundary of Binary Tree/3Sum.py
+++ 545. Boundary of Binary Tree/3Sum.py
@@ -11,22 +11,26 @@
 11                j -= 1
 12                while i < j and nums[i] == nums[i-1]:
 13                    i += 1
-14            elif nums[i] + nums[j] > target:
-15                j -= 1
-16            else:
-17                i += 1
-18
-19        
-20
-21    def threeSum(self, nums: list[int]) -> list[list[int]]:
-22        self.res = []
-23
-24        nums.sort()
-25
-26        for i in range(len(nums)):
-27            if i > 0 and nums[i] == nums[i-1]:
-28                continue 
-29            self.two_sum(nums, i+1, -nums[i])
-30        
-31        return sorted(self.res)
-32        
+14                
+15                while i < j and nums[j] == nums[j+1]:
+16                    j -= 1
+17
+18            elif nums[i] + nums[j] > target:
+19                j -= 1
+20            else:
+21                i += 1
+22
+23        
+24
+25    def threeSum(self, nums: list[int]) -> list[list[int]]:
+26        self.res = []
+27
+28        nums.sort()
+29
+30        for i in range(len(nums)):
+31            if i > 0 and nums[i] == nums[i-1]:
+32                continue 
+33            self.two_sum(nums, i+1, -nums[i])
+34        
+35        return sorted(self.res)
+36        
