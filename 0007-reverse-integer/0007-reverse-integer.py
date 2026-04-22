672d28ae2cf22c6542a211ffac2329a3f484e682
Shruti Joshi <joshishruti009@gmail.com>
2026-04-21 21:19:01 -0700

    Time: 40 ms (87.24%), Space: 19.2 MB (36.35%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+
+        mul = -1 if x < 0 else 1
+
+        x = abs(x)
+
+        num = 0
+        while x > 0:
+            num = (num * 10) + (x % 10)
+            x = x // 10
+        
+        num = num * mul
+
+        if num > (2**31)-1 or num < -(2**31):
+            return 0
+        
+        return num
