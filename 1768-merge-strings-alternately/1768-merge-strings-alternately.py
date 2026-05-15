77568ae0564be78bf2c5f4b07f2dc97a5ce354cb
Shruti Joshi <joshishruti009@gmail.com>
2026-05-15 15:20:46 -0700

    Time: 43 ms (71.84%), Space: 19.2 MB (56.1%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -14,16 +14,9 @@ class Solution:
 
             i += 1
             j += 1
-        
-        if i == word1_len:
-            while j < word2_len:
-                res.append(word2[j])
-                j += 1
-        
-        if j == word2_len:
-            while i < word1_len:
-                res.append(word1[i])
-                i += 1
+
+        res.append(word1[i:])
+        res.append(word2[j:])
         
         return "".join(res)
         
