124bdba0e357b82f60aff8390ffb7ae473ba92eb
Shruti Joshi <joshishruti009@gmail.com>
2026-05-16 15:03:08 -0700

    Time: 94 ms (21.34%), Space: 21.8 MB (93.2%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -5,7 +5,7 @@ class Solution:
         k = 1
 
         for i in range(1, len(nums)):
-            if nums[i-1] == nums[i]:
+            if nums[i] == nums[i-1]:
                 count += 1
             else:
                 count = 0
@@ -13,6 +13,8 @@ class Solution:
             if count <= 1:
                 nums[k] = nums[i]
                 k += 1
-
+        
         return k
+            
+
         
