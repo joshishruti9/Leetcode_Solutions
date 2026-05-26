fb42ad87334b409b0f68aefb033237f77847c8ca
Shruti Joshi <joshishruti009@gmail.com>
2026-05-26 12:19:09 -0700

    Time: 0 ms (100%), Space: 20.5 MB (90.55%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,6 +1,22 @@
 class Solution:
-    def search_left(self, nums, low, high, target):
+    def bisect_left(self, nums, target):
+        low = 0
+        high = len(nums)-1
 
+        while low < high:
+            mid = (low + high) // 2
+
+            if nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid
+        
+        return low
+    
+    def bisect_right(self, nums, target):
+        low = 0
+        high = len(nums)
+        
         while low < high:
             mid = (low + high) // 2
 
@@ -9,7 +25,7 @@ class Solution:
             else:
                 high = mid
         
-        return low
+        return high
 
     def searchRange(self, nums: List[int], target: int) -> List[int]:
 
@@ -17,11 +33,11 @@ class Solution:
             return [-1, -1]
         
 
-        low = bisect.bisect_left(nums, target)
-        high = bisect.bisect_right(nums, target)
+        low = self.bisect_left(nums, target)
+        high = self.bisect_right(nums, target)
 
         start = low if low < len(nums) and nums[low] == target else -1
-        end = high-1 if nums[high-1] == target and start != -1 else -1
+        end = high-1 if high > 0 and nums[high-1] == target else -1
 
         
         return [start, end]
