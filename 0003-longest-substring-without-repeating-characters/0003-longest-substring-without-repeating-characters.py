4527adca42be1a5b160401e4d903b23d87ace259
Shruti Joshi <joshishruti009@gmail.com>
2026-05-29 11:58:29 -0700

    Time: 13 ms (50.65%), Space: 19.2 MB (68.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,27 +1,24 @@
 class Solution:
+    # Take 2 pointers start and end and a dictionary. 
     def lengthOfLongestSubstring(self, s: str) -> int:
-
         hmap = {}
 
-        i = 0
-        j = 0
+        start = 0
+        end = 0
         max_len = 0
 
-        while j < len(s):
-            if s[j] in hmap:
-                max_len = max(max_len, j-i)
-                next_index = hmap[s[j]]
-                
-                while i <= next_index:
-                    hmap[s[i]] -= 1
-                    if hmap[s[i]] == 0:
-                        hmap.pop(s[i])
-                    i += 1
-                
-            hmap[s[j]] = j
-            j += 1
+        while end < len(s):
+            if s[end] in hmap:
+                max_len = max(max_len, end-start)
+                last_index = hmap[s[end]]
+                while start <= last_index:
+                    if hmap[s[start]] <  last_index:
+                        hmap.pop(s[start])
 
-        max_len = max(max_len, j-i)
+                    start += 1
+            
+            hmap[s[end]] = end
+            end += 1
+
+        max_len = max(max_len, end-start)
         return max_len
-
-        
