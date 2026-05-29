0e02e0d273e97d90150529d40387133e2a9382cb
Shruti Joshi <joshishruti009@gmail.com>
2026-05-29 11:39:17 -0700

    Time: 16 ms (13.47%), Space: 30.6 MB (16.03%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,23 +1,35 @@
 class Solution:
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        i = 0
-        j = 0
-        min_len = float("inf")
-        curr_sum = 0
+    
+    # take 2 pointers start and end
+    # increment end and add nums[end] till total < target
+    # if total == target find min_len
+    # else total > target substarct start value from total and increment start
+    #Once end reaches at end == len(nums), substarct start value from total and increment start and check min_len at each step if total == target
+    # return min_len
 
-        while j < len(nums):
-            if curr_sum >= target:
-                min_len = min(min_len, (j-i))
-                curr_sum -= nums[i]
-                i += 1
-            else:
-                curr_sum += nums[j]
-                j += 1
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+
+        end = 0
+        start = 0
+        total = 0
+        min_len = float("inf")
+
+        while end < len(nums):
+            if total < target:
+                total += nums[end]
+                end += 1
+            
+            if total >= target:
+                min_len = min(min_len, end-start)
+                total -= nums[start]
+                start += 1
+
+        if end == len(nums):
+            while total >= target:
+                min_len = min(min_len, end-start)
+                total -= nums[start]
+                start += 1
+            
         
-        if j == len(nums):
-            while i < j and curr_sum >= target:
-                min_len = min(min_len, (j-i))
-                curr_sum -= nums[i]
-                i += 1
-        
-        return min_len if min_len < float("inf") else 0
+        return min_len if min_len != float("inf") else 0
+            
