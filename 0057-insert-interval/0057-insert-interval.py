96097db7e5ae86546b8b8febc5cfc8299377a4e2
Shruti Joshi <joshishruti009@gmail.com>
2026-06-01 23:52:30 -0700

    Time: 3 ms (30.33%), Space: 21.4 MB (56.69%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,67 +1,44 @@
 class Solution:
-    # we can one res list which will be our output
-    # we will put first interval from intervals in res
-    # we will start loop and compare last res interval with curr_interval from intervals, if we can merge them pop last interval from res and add merged interval else add curr_interval
-    # after adding interval compare last res with new_interval and similarly if we can merge, pop and add merged interval or else add new_interval if it fits there
 
-    # we will merge interval if res[-1][1] >= new_interval[1][0] or curr_interval[j][0]
-
+    def update(self, res, target_interval):
+        src_interval = res[-1]
+        if target_interval[0] <= src_interval[1]:
+            # src = [1,10] target = [5,15]
+            res[-1][1] = max(src_interval[1], target_interval[1])
+        else:
+            # src = [1,10] target = [11,15]
+            res.append(target_interval)
 
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-
         res = []
 
         if len(intervals) == 0:
             return [newInterval]
 
-        isNewIntervalUnused = True
-
-        if intervals[0][0] > newInterval[0]:
-            res.append([newInterval[0], newInterval[1]])
-            j = 0
-            isNewIntervalUnused = False
-        else:
-            res.append([intervals[0][0],intervals[0][1]])
+        # make first choice
+        first_interval = intervals[0]
+        if first_interval[0] < newInterval[0]:
+            res.append(first_interval)
             j = 1
+        else:
+            res.append(newInterval)
+            j = 0
+        
+        is_new_interval_inserted = False
 
+        # loop over intervals
         while j < len(intervals):
-            if isNewIntervalUnused:
-                if intervals[j][0] <= newInterval[0]:
-                    if res[-1][1] >= intervals[j][0]:
-                        start, end = res.pop()
-                        res.append([min(start, intervals[j][0]), max(end, intervals[j][1])])
-                    else:
-                        res.append([intervals[j][0], intervals[j][1]])
-                    j += 1
-                else:
-                    if res[-1][1] >= newInterval[0]:
-                        start, end = res.pop()
-                        res.append([min(start, newInterval[0]), max(end, newInterval[1])])
-                        isNewIntervalUnused = False
-                    else:
-                        res.append([newInterval[0], newInterval[1]])
+            candidate_intervals = [intervals[j]]
+            if not is_new_interval_inserted:
+                candidate_intervals.append(newInterval)
+            target_interval = min(candidate_intervals)
+            self.update(res, target_interval)
+            if not is_new_interval_inserted and target_interval == newInterval:
+                is_new_interval_inserted = True
             else:
-                #just check intervals                   
-                if res[-1][1] >= intervals[j][0]:
-                    start, end = res.pop()
-                    res.append([min(start, intervals[j][0]), max(end, intervals[j][1])])
-                else:
-                    res.append([intervals[j][0], intervals[j][1]])
-                
                 j += 1
-        
-        if isNewIntervalUnused:
-            if res[-1][1] >= newInterval[0]:
-                start, end = res.pop()
-                res.append([min(start, newInterval[0]), max(end, newInterval[1])])
-                isNewIntervalUnused = False
-            else:
-                res.append([newInterval[0], newInterval[1]])
 
+        if not is_new_interval_inserted:
+            self.update(res, newInterval)
+        
         return res
-
-
-        
-
-
-        
