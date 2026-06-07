8a51f5c4661538fabbcf88378b3c459d68322b0e
Shruti Joshi <joshishruti009@gmail.com>
2026-06-07 16:30:25 -0700

    Time: 0 ms (100%), Space: 19.3 MB (77.94%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,23 +1,33 @@
 class Solution:
-    def backtrack(self, digits, i, curr_res):
-        if i == len(digits):
+    def traverse(self, digits, curr_index, curr_res):
+
+        if len(curr_res) == len(digits):
             self.res.append("".join(curr_res))
             return
         
-        letters = self.hmap[digits[i]]
+        digit = digits[curr_index]
+        letters = self.hmap[digit]
 
         for letter in letters:
             curr_res.append(letter)
-            self.backtrack(digits, i+1, curr_res)
+            self.traverse(digits, curr_index+1, curr_res)
             curr_res.pop()
+        
+        return
+
 
     def letterCombinations(self, digits: str) -> List[str]:
+
         self.hmap = {'2' :list('abc'),'3':list('def'),'4':list('ghi'),'5':list('jkl'),'6':list('mno'),'7':list('pqrs'),'8':list('tuv'),'9':('wxyz')}
 
         self.res = []
-        self.backtrack(digits, 0, [])
+
+        self.traverse(digits, 0, [])
 
         return self.res
 
-        
+
+
+
+
         
