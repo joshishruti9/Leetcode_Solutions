83bcab1c1946ce0b74397c79e39b3273e295e4bc
Shruti Joshi <joshishruti009@gmail.com>
2026-06-19 11:47:30 -0700

    Time: 15 ms (76.99%), Space: 21.5 MB (66.08%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -4,25 +4,20 @@ class Solution:
         row_len = len(grid)
         col_len = len(grid[0])
 
-        communicating_servers = set()
+        count = 0
+        visited_row = [0] * row_len
+        visited_col = [0] * col_len
 
         for i in range(row_len):
-            visited = set()
             for j in range(col_len):
                 if grid[i][j] == 1:
-                    visited.add((i, j))
+                    visited_row[i] += 1
+                    visited_col[j] += 1
             
-            if len(visited) > 1:
-                communicating_servers.update(visited)
-
-
         for i in range(col_len):
-            visited = set()
             for j in range(row_len):
-                if grid[j][i] == 1:
-                    visited.add((j, i))
+                if grid[j][i] == 1 and (visited_row[j] > 1 or visited_col[i] > 1):
+                    count += 1
             
-            if len(visited) > 1:
-                communicating_servers.update(visited)
-        
-        return len(communicating_servers)
+           
+        return count
