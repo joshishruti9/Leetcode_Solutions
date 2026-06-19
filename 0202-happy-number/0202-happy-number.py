d9d9d4bf9bcc2de1346d85cf878282480175b311
Shruti Joshi <joshishruti009@gmail.com>
2026-06-19 13:48:33 -0700

    Time: 0 ms (100%), Space: 19.5 MB (5.82%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,15 +1,14 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        
-        if n == 1 or n == 7:
+        if n == 1 or n==7:
             return True
         
         if n <= 9:
             return False
         
-        total = 0
+        num = 0
         while n > 0:
-            total += ((n % 10) * (n % 10))
+            num += (n % 10) ** 2
             n = n // 10
-        
-        return self.isHappy(total)
+
+        return self.isHappy(num)
