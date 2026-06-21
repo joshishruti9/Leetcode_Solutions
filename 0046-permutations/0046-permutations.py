1ce4bcec6830b6cc4d738720676e86de937617b4
Shruti Joshi <joshishruti009@gmail.com>
2026-06-21 12:36:24 -0700

    Time: 0 ms (100%), Space: 19.3 MB (87.02%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,23 +1,21 @@
 class Solution:
-    def traverse(self, curr_res, nums, visited):
-
-        if len(nums) == len(curr_res):
-            self.res.append(curr_res[:])
-            return
-
+    def traverse(self, nums, curr_res, visited):
+        if len(curr_res) == len(nums):
+            self.output.append(curr_res[:])
+        
         for i in range(len(nums)):
             if nums[i] not in visited:
-                visited.add(nums[i])
                 curr_res.append(nums[i])
-                self.traverse(curr_res, nums, visited)
-                curr_res.pop()
+                visited.add(nums[i])
+                self.traverse(nums, curr_res, visited)
                 visited.remove(nums[i])
+                curr_res.pop()
         
+        return
 
     def permute(self, nums: List[int]) -> List[List[int]]:
 
-        self.res = []
-
-        self.traverse([], nums, set()) 
+        self.output = []
+        self.traverse(nums, [], set())
+        return self.output
         
-        return self.res
