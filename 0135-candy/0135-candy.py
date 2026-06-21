6f9e60d597033fa726169b7637d7f2535417f479
Shruti Joshi <joshishruti009@gmail.com>
2026-06-21 12:06:58 -0700

    Time: 14 ms (74.86%), Space: 21.6 MB (13.36%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,37 +1,16 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
-        candies = [1 for i in range(len(ratings))]
+        candies = [1 for i in range(n)]
 
-        sorted_ratings = sorted(ratings)
-
-        index_map = {}
-
-        for index, rating in enumerate(ratings):
-            if rating not in index_map:
-                index_map[rating] = [index]
-            else:
-                index_map[rating].append(index)
+        #check only left
+        for i in range(1, n):
+            if ratings[i-1] < ratings[i] :
+                candies[i] = candies[i-1] + 1
         
-        for rating in sorted_ratings:
-            for curr_index in index_map[rating]:
+        #check right and left:
+        for i in range(n-2, -1, -1):
+            if ratings[i+1] < ratings[i] :
+                candies[i] = max(candies[i], candies[i+1]+1)
 
-                left_small = curr_index > 0 and ratings[curr_index] > ratings[curr_index-1]
-                right_small = curr_index < n-1 and ratings[curr_index] > ratings[curr_index+1]
-
-                if left_small and right_small:
-                    candies[curr_index] = max(candies[curr_index+1], candies[curr_index-1]) + 1
-                elif left_small:
-                    candies[curr_index] = candies[curr_index-1] + 1
-                elif right_small:
-                    candies[curr_index] = candies[curr_index+1] + 1
-                else:
-                    continue
-                
-
-
-        
         return sum(candies)
-                    
-
-        
