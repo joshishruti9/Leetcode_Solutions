147f13c925405f90cac14eac0a8c27635b9427ea
Shruti Joshi <joshishruti009@gmail.com>
2026-06-21 18:10:22 -0700

    Time: 16 ms (64.8%), Space: 19.8 MB (8.24%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,15 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
-        counter = Counter(magazine)
+        magazine_counter = Counter(magazine)
 
         for letter in ransomNote:
-            if letter in counter and counter[letter] > 0:
-                counter[letter] -= 1
+            if letter in magazine_counter:
+                magazine_counter[letter] -= 1
+                if magazine_counter[letter] == 0:
+                    magazine_counter.pop(letter)
             else:
                 return False
         
         return True
+        
