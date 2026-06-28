c996a39d0f80bc967f8bcc69b44cd08e3ae65135
Shruti Joshi <joshishruti009@gmail.com>
2026-06-27 18:58:20 -0700

    Time: 8 ms (26.58%), Space: 19.1 MB (88.48%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,22 +1,19 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
 
-        symbol_num_dict = {"I" : 1, "IV" : 4, "V" : 5, "IX" : 9, "X":10, "XL":40, "L":50, "XC": 90, "C": 100, "CD": 400, "D": 500, "CM": 900, "M":1000}
+        symbol_num_dict = {"I":1, "V":5, "X":10, "L":50, "C": 100, "D": 500, "M":1000}
 
-        res = 0
-        i = 0
+        res = symbol_num_dict[s[len(s)-1]]
+        i = len(s)-2
 
-        while i < len(s)-1:
-            if s[i:i+2] in symbol_num_dict:
-                res += symbol_num_dict[s[i:i+2]]
-                i += 2
+        while i >= 0:
+            if symbol_num_dict[s[i]] < symbol_num_dict[s[i+1]]:
+                res -= symbol_num_dict[s[i]]
+                i -= 1
             else:
                 res += symbol_num_dict[s[i]]
-                i += 1
+                i -= 1
         
-        if i == len(s)-1:
-            res += symbol_num_dict[s[i]]
-
         return res
 
 
