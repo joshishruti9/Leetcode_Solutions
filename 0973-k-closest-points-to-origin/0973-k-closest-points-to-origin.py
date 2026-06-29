b8f29e7dfa5154573a2e8d22f58e8890188841f7
Shruti Joshi <joshishruti009@gmail.com>
2026-06-28 23:00:17 -0700

    Time: 80 ms (24.34%), Space: 25 MB (43.94%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,35 +1,20 @@
+from heapq import heappush, heappop
 class Solution:
-    def calculate_distance(self, x, y, x1, y1):
-        
-        dist = ((abs(x1-x)) ** 2 + (abs(y1-y)) ** 2) ** 0.5
-        return dist
-    
+    def calculate_distance(self, x, y):
+        return ((x ** 2) + (y ** 2)) ** 0.5
+
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
 
-        max_heap = []
+        closest_points = []
+
+        for point in points:
+            dist = self.calculate_distance(point[0], point[1])
+            heappush(closest_points, (dist, point))
         
-        #to represent the postion of warehouse
-        x = 0
-        y = 0
-        
-        for x1, y1 in points:
-            dist = self.calculate_distance(x, y, x1, y1)
-            
-            if len(max_heap) < k:
-                heappush(max_heap, (-dist, x1, y1))
-            else:
-                popped_dist, popped_x1, popped_y1 = heappop(max_heap)
-                
-                popped_dist = popped_dist * -1
-                
-                if dist < popped_dist:
-                    heappush(max_heap, (-dist, x1, y1))
-                else:
-                    heappush(max_heap, (-popped_dist, popped_x1, popped_y1))
-                    
         res = []
         for i in range(k):
-            res.append((max_heap[i][1], max_heap[i][2]))
-            
+            dist, point = heappop(closest_points)
+            res.append(point)
+        
         return res
         
