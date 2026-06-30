68cd4d19ce7425ac549ea39b3788d95d5b5c99b0
Shruti Joshi <joshishruti009@gmail.com>
2026-06-30 00:12:38 -0700

    Time: 0 ms (100%), Space: 19.5 MB (39.36%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,30 +1,23 @@
 class Solution:
-    def bisect_left(self, low, high, nums, target):
-        
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
         while low < high:
             mid = (low + high) // 2
-
-            if nums[mid] == target:
+            if target == nums[mid]:
                 return mid
             
-            #check whether left half is sorted or not?
-            if nums[mid] >= nums[low]:
+            if nums[low] <= nums[mid]:
                 if nums[low] <= target < nums[mid]:
                     high = mid
                 else:
                     low = mid + 1
-            
             else:
+                # target is to the right of mid
                 if nums[mid] < target <= nums[high]:
                     low = mid + 1
                 else:
                     high = mid
-                
-        return low
 
-            
-    def search(self, nums: List[int], target: int) -> int:
-
-        index = self.bisect_left(0, len(nums)-1, nums, target)
-        return index if nums[index] == target else -1
-        
+        return low if nums[low] == target else -1
