d3e29e4412fb7ab965d3487c53541843e39ffd93
Shruti Joshi <joshishruti009@gmail.com>
2026-06-29 22:49:09 -0700

    Time: 57 ms (23.85%), Space: 20.1 MB (32.68%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,39 +1,32 @@
 class Solution:
-    def backtrack(self, curr_s1, curr_s2, curr_s3, s1, s2, s3):
+    def backtrack(self, s1, s2, s3, i, j):
+        if (i, j) in self.memo:
+            return self.memo[(i, j)]
 
-        if (curr_s1, curr_s2, curr_s3) in self.memo:
-            return self.memo[(curr_s1, curr_s2, curr_s3)]
-
-        if curr_s1 == len(s1) and curr_s2 == len(s2) and curr_s3 == len(s3):
-            self.memo[(curr_s1, curr_s2, curr_s3)] = True
+        k = i + j
+        if i == len(s1) and j == len(s2) and k == len(s3):
+            self.memo[(i, j)] = True
             return True
         
-        if curr_s1 < len(s1) and s1[curr_s1] == s3[curr_s3]:
-            if self.backtrack(curr_s1+1, curr_s2, curr_s3+1, s1, s2, s3):
-                self.memo[(curr_s1, curr_s2, curr_s3)] = True
-                return True
-          
-        if curr_s2 < len(s2) and  s2[curr_s2] == s3[curr_s3]:
-            if self.backtrack(curr_s1, curr_s2+1, curr_s3+1, s1, s2, s3):
-                self.memo[(curr_s1, curr_s2, curr_s3)] = True
-                return True
-            
-        self.memo[(curr_s1, curr_s2, curr_s3)] = False
-        
-        return self.memo[(curr_s1, curr_s2, curr_s3)]
-        
-
-    
-
-    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) != len(s3):
+        if (i < len(s1) and k < len(s3) and s1[i] != s3[k]) and (j < len(s2) and k < len(s3) and s2[j] != s3[k]):
+            self.memo[(i, j)] = False
             return False
         
-        self.memo = {}
+        s1_res = s2_res = False
+
+        if i < len(s1) and k < len(s3) and s1[i] == s3[k]:
+            s1_res = self.backtrack(s1, s2, s3, i+1, j)
         
-        self.backtrack(0, 0, 0, s1, s2, s3)
+        if j < len(s2) and k < len(s3) and s2[j] == s3[k]:
+            s2_res = self.backtrack(s1, s2, s3, i, j+1)
+        
+        self.memo[(i, j)] = s1_res or s2_res
+        
+        return self.memo[(i, j)]
 
-        return self.memo[(0, 0, 0)]
 
-
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        self.memo = {}
+        self.backtrack(s1, s2, s3, 0, 0)
+        return self.memo[(0 ,0)]
         
