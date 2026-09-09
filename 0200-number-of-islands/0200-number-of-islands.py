f0f19a74d91bc2461274d3381dcc2bdca4fba94a
Shruti Joshi <joshishruti009@gmail.com>
2026-09-08 19:33:36 -0700

    Time: 226 ms (93.08%), Space: 26.8 MB (22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,32 +1,35 @@
-from queue import deque
+from collections import deque
 
 class Solution:
-    def find_neighbours(self, i, j, grid, m, n, queue, visited):
-        dirs = [(0,1),(1,0),(-1,0),(0,-1)]
+    def traverse(self, m, n, queue, visited, grid):
 
-        for di, dj in dirs:
-            if 0 <= i+di < m and 0 <= j+dj < n and (i+di, j+dj) not in visited and grid[i+di][j+dj] == '1' :
-                queue.append((i+di,j+dj))
-                visited.add((i+di, j+dj))
-    
+        dirs = [(0,1), (-1,0), (0,-1), (1,0)]
+
+        while queue:
+            i , j = queue.popleft()
+            for di, dj in dirs:
+                if 0 <= i+di < m and 0 <= j+dj < n and (i+di, j+dj) not in visited and grid[i+di][j+dj] == "1":
+                    queue.append((i+di, j+dj))
+                    visited.add((i+di, j+dj))
+
+
     def numIslands(self, grid: List[List[str]]) -> int:
-        count = 0
+
         queue = deque()
         visited = set()
 
         m = len(grid)
         n = len(grid[0])
 
+        count = 0
+
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1' and (i,j) not in visited:
-                    queue.append((i,j))
-                    visited.add((i,j))
-                    while queue:
-                        r, c = queue.popleft()
-                        self.find_neighbours(r, c, grid, m, n, queue, visited)
-
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    queue.append((i, j))
+                    visited.add((i, j))
+                    self.traverse(m, n, queue, visited, grid)
                     count += 1
-
+        
         return count
         
